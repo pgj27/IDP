9da9582acd9c9bc43c926b2bb5d3d88a4bf4957c
Philip Jennings <philip.jennings@createc.co.uk>
2019-03-06 11:01:22 +0000

    Steering control over serial

--- comms_test.py
+++ comms_test.py
@@ -1,22 +1,48 @@
 import serial
 import time
 import struct
+import numpy as np
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while True:
+
+def wait_for_message(msg):
     line = ser.readline()
-    response_rec = b'Got command\r\n'
-    while(line != response_rec):
-        print("heard: {}".format(str(line, 'utf-8')))
+    while(line != msg):
+        print("heard: {}".format(str(line, 'utf-8', errors='ignore')))
+        time.sleep(0.1)
+        line = ser.readline()
+
+while True:
+    ready_for_path = b'Following path\r\n'
+    distance_response = b'Distance received\r\n'
+    finish_response = b'Finished moving\r\n'
+    ready_for_steering = b'Constant movement\r\n'
+    steering_response = b'Steering command\r\n'
+
+    wait_for_message(ready_for_path)
+
+    line = ser.readline()
+    while(line != distance_response):
         ser.write(str.encode("cf"))
         ser.write(struct.pack("h", 100))
-        line = ser.readline()
+        print("Sent cf 100")
+        print("heard: {}".format(str(line, 'utf-8', errors='ignore')))
+        ser.write(str.encode("cf"))
         time.sleep(0.1)
+        line = ser.readline()
     print("Sent command successfully")
 
-    response_fin = b'Finished moving\r\n'
-    while(line != response_fin):
-        print("heard: {}".format(str(line, 'utf-8')))
+    wait_for_message(ready_for_steering)
+    print("Feedback")
+    i = 0
+    while(line != finish_response):
+        steering = int(50 * np.cos(i * np.pi / 10))
+        i += 1
+        ser.write(str.encode("cs"))
+        ser.write(struct.pack("h", steering))
+        wait_for_message(steering_response)
+        print("Sent cs {}".format(steering))
+        print("heard: {}".format(str(line, 'utf-8', errors='ignore')))
         time.sleep(0.1)
         line = ser.readline()
     print("Success")
