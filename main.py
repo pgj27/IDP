5a13f76b012b2e285fa5ec99597fcd4a257f643c
Philip Jennings <philip.jennings@createc.co.uk>
2019-03-08 22:28:21 +0000

    Added position smoothing and angle tracking

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ class RobotControl:
     conts = []
     mask = []
     pos = (0, 0)
+    pos_buffer = []
+    pos_buffer_location = 0
+    angle = 0
     cells = []
     #ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -92,36 +95,58 @@ class RobotControl:
         maskBodyOpen = cv2.morphologyEx(maskBody, cv2.MORPH_OPEN, kernelOpen)
         maskBodyClose = cv2.morphologyEx(maskBodyOpen, cv2.MORPH_CLOSE, kernelCloseBody)
         maskMarking = cv2.inRange(img, lowerBoundMarking, upperBoundMarking)
-        #maskMarkingOpen = cv2.morphologyEx(maskMarking, cv2.MORPH_OPEN, kernelOpen)
         maskMarkingClose = cv2.morphologyEx(maskMarking, cv2.MORPH_CLOSE, kernelCloseMarking)
         maskFinal = np.uint8((0.5 * maskMarkingClose + 0.5 * maskBodyClose) / 255)
         conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         to_remove = []
         for i in range(len(conts)):
-            print(conts[i][int(len(conts[i])/2)])
             dist_to_pos = np.linalg.norm(conts[i][int(len(conts[i])/2)][0] - self.pos)
             if len(conts[i]) < lengthLower or (dist_to_pos > 50 and self.pos != (0, 0)):
                 to_remove.append(i)
         conts = np.delete(conts, to_remove, 0)
 
+        # Get robot orientation
+
         # If robot is found, update position to average point location
         if len(conts) > 0:
             x_sum = 0
             y_sum = 0
-            sum = 0
+            angle_sum = 0
+            n = 0
             for c in conts:
+                p, m, angle = cv2.fitEllipse(c)
+                angle_sum += angle * np.pi / 180
                 x_sum += c[int(len(c)/2)][0][0]
                 y_sum += c[int(len(c)/2)][0][1]
-                sum += 1
-            self.pos = (int(x_sum / sum), int(y_sum / sum))
+                n += 1
+
+            # Average angles to get robot heading
+            self.angle = angle_sum / n
+
+            # Update position smoothing buffer
+            if len(self.pos_buffer) < 5:
+                self.pos_buffer.append((int(x_sum / n), int(y_sum / n)))
+            else:
+                self.pos_buffer[self.pos_buffer_location] = (int(x_sum / n), int(y_sum / n))
+            self.pos_buffer_location = (self.pos_buffer_location + 1) % 5
             print("pos: {}".format(self.pos))
 
-        return conts, maskMarking#255 * maskFinal
+        # Smooth robot position
+        x_sum = 0
+        y_sum = 0
+        n = 0
+        for i in self.pos_buffer:
+            x_sum += i[0]
+            y_sum += i[1]
+            n += 1
+        self.pos = (int(x_sum / n), int(y_sum / n))
+
+        return conts, 255 * maskFinal
 
     def wait_for_message(self, msg):
         line = self.ser.readline()
         while line != msg:
-            print("Waiting for {}, heard {}".format(msg. line))
+            print("Waiting for {}, heard {}".format(msg, line))
             time.sleep(0.01)
             line = self.ser.readline()
 
@@ -152,16 +177,14 @@ class RobotControl:
         if ret == 0:
             return ret
     
-        # Our operations on the frame come here
+        # Convert frame to HSV (imshow used for calibration)
         frame = cv2.resize(frame, (640, 480))
         frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-        # Display the resulting frame
-        cv2.imshow('h',frameHSV[:,:,0])
-        cv2.imshow('s',frameHSV[:,:,1])
-        cv2.imshow('v',frameHSV[:,:,2])
+        #cv2.imshow('h',frameHSV[:,:,0])
+        #cv2.imshow('s',frameHSV[:,:,1])
+        #cv2.imshow('v',frameHSV[:,:,2])
 
-        # Do first frame processing
+        # Do first frame processing (find cells, scale)
         if self.first_frame:
             self.first_frame = False
             self.conts, self.mask = self.process_first_frame(frameHSV)
@@ -169,9 +192,11 @@ class RobotControl:
         # Find robot
         self.conts, self.mask = self.find_robot(frameHSV)
     
-        # Show robot position and corresponding points
+        # Show robot position and heading (contours for corresponding points)
         cv2.drawContours(frame, self.conts, -1, (255, 0, 0), 3)
         cv2.circle(frame, self.pos, 20, (0, 255, 0), 3)
+        arrow = (int(self.pos[0] + 50 * np.cos(self.angle)), int(self.pos[1] + 50 * np.sin(self.angle)))
+        cv2.line(frame, self.pos, arrow, (0, 0, 255), 5)
         # Show filtered mask (for robot)
         cv2.imshow("mask", self.mask)
         # Display camera frame with overlay
